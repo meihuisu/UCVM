df7f16163d248b5085f5660d0a6f1c7af5faf238
David Gill <davidgil@usc.edu>
2017-03-07 11:09:20 -0800

    Additional add python3 interpreter to setup.py to prevent hash-bang limits in Linux fix.

--- setup.py
+++ setup.py
@@ -369,7 +369,7 @@ paths.append(os.path.join(_LOCAL_LIBRARY_PATH, "euclid3", "lib"))
 os.environ[environment_variable] = ":".join(paths)
 
 # Run the tests.
-execute([os.path.join(_LOCAL_SCRIPT_PATH, "ucvm_run_tests"), "-t"], env=os.environ)
+execute(["python3", os.path.join(_LOCAL_SCRIPT_PATH, "ucvm_run_tests"), "-t"], env=os.environ)
 
 print("Thank you for installing UCVM. The installation is now complete. To learn more about")
 print("UCVM, please run the command ucvm_help.")
